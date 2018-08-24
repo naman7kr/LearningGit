da3b2ae7632739e63cbdac578c88fe4eee951ce3
Naman <naman7kr@gmail.com>
2018-08-24 22:47:41 +0530

    revert successful

--- GitConflicts/f1.c
+++ GitConflicts/f1.c
@@ -3,10 +3,7 @@
 int main(){
 	int t;
 	scanf("%d",&t);
-<<<<<<< HEAD
-	printf("How you Doing Naman and Stan%d",t);
-=======
-	printf("How you Doing Stan and Naman%d",t);
->>>>>>> branch1
+	printf("How you Doing Naman%d",t);
+	printf("How you Doing Stan%d",t);
 	return 0;
 }
